cc24b456fe3475efd22779d608d38417ec1d2660
patorma <pocontreras@ing.ucsc.cl>
2020-09-18 01:01:42 -0300

    se termino lo de la introduccion a contenido en JS

--- js/script2.js
+++ js/script2.js
@@ -3,8 +3,10 @@ const title = document.querySelector("h1");
 
 //title.setAttribute("id", "title");
 // title.id = "title";
-
+// title.textContent = "Hola mamá, <em>estoy en clases</em>";
+title.innerHTML = `Hola mamá, 
+<em> estoy en clase</em>`;
 // title.classList.add("main-title", "title-front-page");
-c(title.textContent);
-c(title.innerHTML);
-c(title.outerHTML);
+// c(title.textContent);
+// c(title.innerHTML);
+// c(title.outerHTML);
