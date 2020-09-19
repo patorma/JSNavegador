9ff576af31f009afa56f20b269a08075871af1de
patorma <pocontreras@ing.ucsc.cl>
2020-09-18 21:11:37 -0300

    se vio el metodo addEventListener()

--- js/script4.js
+++ js/script4.js
@@ -1,3 +1,12 @@
-function holaMundo() {
-    alert("Hola mundo");
-}
+const title = document.getElementById("title");
+
+// title.addEventListener('eventName',eventHandler)
+const holaMundo = (e) => alert(e.target.textContent);
+//cuando se quiere pasar parametros usar funcion anonima
+title.addEventListener("click", (e) => {
+    holaMundo(e);
+});
+
+document.querySelector("button").addEventListener("click", (e) => {
+    holaMundo(e);
+});
