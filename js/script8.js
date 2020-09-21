4107902afd7c46d8b3e90af2e2afee05ef531dee
patorma <pocontreras@ing.ucsc.cl>
2020-09-20 21:07:05 -0300

    se vio lo de delegacion de eventos

--- js/script8.js
+++ js/script8.js
@@ -1,8 +1,22 @@
-document.querySelectorAll('div').forEach(el => {
-    el.addEventListener('click', (e) => {
-        console.log(`Hiciste click en el div ${e.target.id}`)
+// document.querySelectorAll("div").forEach((el) => {
+//     el.addEventListener("click", (e) => {
+//         console.log(`Hiciste click en el div ${e.target.id}`);
 
-        //detener que se propague el evento
-        e.stopPropagation()
-    })
-})
+//         //detener que se propague el evento
+//         e.stopPropagation();
+//     });
+// });
+
+const gallery = document.getElementById("gallery");
+
+//se escucha el evento en el papá
+gallery.addEventListener("click", (e) => {
+    //detectar en que imagen e hace click
+    //se guardara en t ele elemento que se hizo click
+    const t = e.target,
+        //se crea una galeria de imagenes
+        //dentro de las galarias busco todas las iamgene,s
+        images = Array.from(gallery.querySelectorAll("img")),
+        i = images.indexOf(t);
+    console.log(`Hiciste click en la imagen ${i + 1}`);
+});
