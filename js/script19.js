e6a8372447ffbe85c129ef3fbcde8d5644f61c37
patorma <pocontreras@ing.ucsc.cl>
2020-09-24 01:24:52 -0300

    se vio el objeto window con sus objetos y metodos

---
+++ js/script19.js
@@ -0,0 +1,15 @@
+//metodos del objeto window
+// alert("hola mundo");
+
+// let userResponse = confirm("Estas seguro?");
+
+// if (userResponse) {
+//     alert("el usuario esta seguro");
+// } else {
+//     alert("el usuario no esta seguro");
+// }
+
+// let userAge = prompt("Cual es tu edad?");
+
+// alert(`La edad del usuario es ${userAge}`);
+window.open("https://ed.team", "EDteam", "width=200,height=400");
