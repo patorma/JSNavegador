9ef31ffe04e2c8c68beb3bdf64b230f713ede536
patorma <pocontreras@ing.ucsc.cl>
2020-09-23 23:52:29 -0300

    se termino con lo de CSSOM con un ejemplo

---
+++ js/script18.js
@@ -0,0 +1,7 @@
+const title = document.querySelector("h1");
+const description = document.querySelector("p");
+
+//calcula los estilos de los elementos
+//incluso se puede calcular la posicion
+// console.log(getComputedStyle(document.body).position);
+console.log(getComputedStyle(document.body).display);
