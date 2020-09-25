6233cf9a85ad3c234a414ece937a064714b543de
patorma <pocontreras@ing.ucsc.cl>
2020-09-25 01:19:35 -0300

    se vio Timers

--- js/script21.js
+++ js/script21.js
@@ -6,7 +6,6 @@
 //     }
 //  }
 
-
 // calcular links externos de una pagina
 // for(let link of links){
 //     if(!link.href.includes(location.host)){
@@ -17,7 +16,38 @@
 
 // location.pathname  muestra ruta interna luego del dominio
 
-
 // if(location.pathname.includes('cursos')){
 //     document.body.classList.add('cursos-page')
 // }
+
+//setTimeout()
+// setTimeout(() => {
+//     alert('Hola mundo')
+// }, 3000)
+
+//ejemplo de setInterval()
+// let contador = 0;
+// const saludarMuchasVeces = setInterval(() => {
+//     contador++;
+//     if (contador > 1) {
+//         console.log(`Hola ${contador} veces`);
+//     } else {
+//         console.log(`Hola ${contador} vez`);
+//     }
+
+//     if (contador === 20) {
+//         clearInterval(saludarMuchasVeces);
+//     }
+// }, 1000);
+
+// console.log(new Date().toLocaleString());
+// console.log(new Date().toString());
+//devuelve horas, minutos y segundos
+console.log(new Date().toLocaleTimeString());
+
+setInterval(() => {
+    const clock = document.getElementById("clock");
+    if (clock) {
+        clock.textContent = new Date().toLocaleTimeString();
+    }
+}, 1000);
