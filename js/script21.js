1f552301d8e4561efdf31bd88621347c6451034d
patorma <pocontreras@ing.ucsc.cl>
2020-09-24 23:03:32 -0300

    objeto location

---
+++ js/script21.js
@@ -0,0 +1,23 @@
+// calcular links internos de una pagina
+// for(let link of links){
+//     if(link.href.includes(location.host)){
+//         link.style.background = 'yellow'
+//         console.log(link)
+//     }
+//  }
+
+
+// calcular links externos de una pagina
+// for(let link of links){
+//     if(!link.href.includes(location.host)){
+//         link.style.background = 'yellow'
+//         console.log(link)
+//     }
+//  }
+
+// location.pathname  muestra ruta interna luego del dominio
+
+
+// if(location.pathname.includes('cursos')){
+//     document.body.classList.add('cursos-page')
+// }
