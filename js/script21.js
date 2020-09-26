9f8de1013aba2df6eff5d00c3e62ecddf74445e4
patorma <pocontreras@ing.ucsc.cl>
2020-09-25 21:02:26 -0300

    se termino el curso de js en el navegador

--- js/script21.js
+++ js/script21.js
@@ -43,11 +43,32 @@
 // console.log(new Date().toLocaleString());
 // console.log(new Date().toString());
 //devuelve horas, minutos y segundos
-console.log(new Date().toLocaleTimeString());
+// console.log(new Date().toLocaleTimeString());
 
-setInterval(() => {
-    const clock = document.getElementById("clock");
-    if (clock) {
-        clock.textContent = new Date().toLocaleTimeString();
-    }
-}, 1000);
+// setInterval(() => {
+//     const clock = document.getElementById("clock");
+//     if (clock) {
+//         clock.textContent = new Date().toLocaleTimeString();
+//     }
+// }, 1000);
+// const now = new Date();
+// console.log(now);
+
+// const aniversario = new Date(2000, 4, 21, 20, 30, 12);
+// console.log(aniversario);
+
+// const aniversario = new Date("Fri Sep 25 2020 01:28:08 ");
+// console.log(aniversario);
+// const timeZoneOffset = new Date().getTimezoneOffset();
+// console.log(timeZoneOffset);
+
+// const now = new Date();
+// console.log(now);
+
+const birthday = new Date(1981, 4, 6);
+const now = new Date(2020, 8, 25);
+
+const getSeconds = (ms) => Math.round(ms / 1000);
+const getMinutes = (ms) => getSeconds(ms) / 60;
+
+console.log(getMinutes(now - birthday));
